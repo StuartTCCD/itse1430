52be3bc2c605225c71ae56740a1d7d8a77f06b30
StuartTCCD <77857638+StuartTCCD@users.noreply.github.com>
2021-04-20 13:53:05 -0500

    Add files via upload

---
+++ labs/Lab4/CharacterCreator/ObjectValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CharacterCreator
+{
+    public class ObjectValidator
+    {
+        public List<ValidationResult> TryValidate ( IValidatableObject value )
+        {
+            var context = new ValidationContext(value);
+            var errors = new List<ValidationResult>();
+
+            Validator.TryValidateObject(value, context, errors, true);
+
+            return errors;
+        }
+    }
+}
